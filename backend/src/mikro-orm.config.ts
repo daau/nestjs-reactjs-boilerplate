68c25bb3b926de75f63859dd766bfd5f7adf6be5
Daniel Au <dakmau@gmail.com>
2024-12-28 12:30:08 -0800

    Setup MikroORM

---
+++ backend/src/mikro-orm.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from '@mikro-orm/postgresql';
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { Migrator } from '@mikro-orm/migrations';
+import { EntityGenerator } from '@mikro-orm/entity-generator';
+import { SeedManager } from '@mikro-orm/seeder';
+
+export default defineConfig({
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  dbName: process.env.POSTGRES_DB,
+  entities: ['dist/**/*.entity.js'],
+  entitiesTs: ['src/**/*.entity.ts'],
+  debug: true,
+  metadataProvider: TsMorphMetadataProvider,
+  extensions: [Migrator, EntityGenerator, SeedManager],
+});
